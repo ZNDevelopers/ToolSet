5bb29ec89f5a00468388a36170e584d99d46403c
Zach Barham <me@zoweb.me>
2017-09-24 16:13:16 +1000

    Added on-join update checker

---
+++ src/main/java/com/zndevs/toolset/commands/ToolSetCommandCommand.java
@@ -0,0 +1,9 @@
+package com.zndevs.toolset.commands;
+
+/**
+ * Class com.zndevs.toolset.commands.ToolSetCommandCommand created by zoweb
+ * for ToolSet
+ */
+
+public class ToolSetCommandCommand {
+}
---
+++ src/main/java/com/zndevs/toolset/events/ToolSetJoinEvent.java
@@ -0,0 +1,13 @@
+package com.zndevs.toolset.events;
+
+import org.bukkit.event.Listener;
+
+/**
+ * Class com.zndevs.toolset.events.PlayerJoinEvent created by zoweb
+ * for ToolSet
+ */
+
+public class PlayerJoinEvent implements Listener {
+    @Override
+    onPlayerJoin()
+}
